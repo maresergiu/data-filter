afe62280fde55f1d3489fc0e1b36df2e91a9fcf7
Sergiu Mare <mare.sergiu@yahoo.com>
2020-07-25 19:23:50 +0300

    feature/2-people-list: styling of the pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,17 +1,42 @@
 <template>
-  <ul v-if="pages.length > 1" class="pagination cf">
-    <li
-      v-for="page in pages"
-      :key="`key-${page}`"
-      class="pagination-element float-left"
-      :class="{ active: activePage === page }"
-    >
-      <button type="button" class="cta-smp" @click="handleClickCta(page)">{{ page }}</button>
-    </li>
-  </ul>
+  <div class="text-center">
+    <ul v-if="pages.length > 1" class="pagination cf inline-bl">
+      <li class="pagination-element float-left">
+        <button
+          type="button"
+          class="cta cta-smp"
+          :class="{ 'disabled': activePage === 1 }"
+          @click="handleClickCta(1)"
+        >first page</button>
+      </li>
+      <li
+        v-for="page in pages"
+        :key="`key-${page}`"
+        class="pagination-element float-left"
+        :class="{ active: activePage === page }"
+      >
+        <button
+          v-if="activePage -1 === page || activePage === page || activePage + 1 === page"
+          type="button"
+          class="cta cta-smp"
+          @click="handleClickCta(page)"
+        >{{ page }}</button>
+      </li>
+      <li class="pagination-element float-left">
+        <button
+          type="button"
+          class="cta cta-smp"
+          :class="{ 'disabled': activePage === pages[pages.length - 1] }"
+          @click="handleClickCta(pages[pages.length - 1])"
+        >last page</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import "../scss/components/pagination.scss";
+
 export default {
   name: "Pagination",
   props: {
